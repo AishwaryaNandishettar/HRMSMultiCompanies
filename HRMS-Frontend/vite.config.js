047de71c7d41cd7677eac15df8402cc6b339e58a
Aishwarya Nandishettar <aishushettar95@gmail.com>
2026-05-28 18:27:10 +0500

    fixed vite env

--- HRMS-Frontend/vite.config.js
+++ HRMS-Frontend/vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
     port: 5176,
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_BASE_URL|| "http://localhost:8082",
+        target: env.VITE_API_BASE_URL || "http://localhost:8082",
         changeOrigin: true,
         secure: false,
       },
