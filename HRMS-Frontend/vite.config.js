278e63d57970fbd3828ebe8c4c399ef8d2653cfd
Aishwarya Nandishettar <aishushettar95@gmail.com>
2026-05-28 18:15:20 +0500

    chaged configuratio set url

--- HRMS-Frontend/vite.config.js
+++ HRMS-Frontend/vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
     port: 5176,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_BASE_URL || "http://localhost:8082",
+        target: import.meta.env.VITE_API_BASE_URL|| "http://localhost:8082",
         changeOrigin: true,
         secure: false,
       },
